a8e8f5017610386c9bd28f7f797d96e0f13646d2
yandiswandarane@gmail.com <yandiswandarane@gmail.com>
2024-04-04 14:16:16 +0200

    arrays

---
+++ javaScript/arrays.js
@@ -0,0 +1,11 @@
+let courses = ["C++","java","Python","Angular","React"] //pre-defined array
+
+//Add values to an array
+courses.push(["c#",'assembly'])
+console.log(courses)
+
+let faculty = courses.toString()
+console.log(faculty.toUpperCase())
+//Check if the array is a array object
+console.log(`The courses is a an object of an Array, ${courses instanceof Array}`)
+
